9bf387b805e578ec476daab5308b289f380c7f0c
Arbias <k.arbias@live.com>
2019-11-11 17:50:20 +0100

    Added a function in JS File to show error

--- app.js
+++ app.js
@@ -26,3 +26,13 @@ function calculateResults(e){
 
     e.preventDefault();
 }
+
+function showError(error){
+    const errorDiv = document.createElement('div');
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+    errorDiv.className = 'alert alert-danger';
+
+    errorDiv.appendChild(document.createTextNode(error));
+    card.insertBefore(errorDiv, heading);
+}
